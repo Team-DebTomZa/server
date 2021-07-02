5fa44a69ecb0c5c76e4ee19714aa8ebdc9665ca6
Tom Virgo <tomcvirgo@gmail.com>
2021-07-02 17:45:29 +0100

    add / route and index.js entry point

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-//APP CODE
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+
+//MIDDLEWARE
+app.use(express.json());
+app.use(cors());
+
+//ROUTES
+app.get('/', (req,res) => {
+    res.json('Server is up and running - woop!!');
+});
+
+module.exports = { app };
+
+
+
+
