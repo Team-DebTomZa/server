7460a57f2880349236933aadf54df40e11562952
Tom Virgo <tomcvirgo@gmail.com>
2021-07-05 12:33:53 +0100

    add date to journal model

--- models.js
+++ models.js
@@ -1,11 +1,14 @@
 class Journal{
     constructor(id, title, content){
+        let date = new Date();
         this.id = id;
         this.title = title;
         this.content = content;
         this.emojis = [0, 0, 0]; //array with each element representing number of that emoji reactions the journal has
-        this.comments = []; 
+        this.comments = [];
+        this.date = date.toLocaleDateString();
     }
 }
+
 
 module.exports = { Journal };
