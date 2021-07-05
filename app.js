c67899f69c1ef74699f29b6f8f07cbe03c420e02
Tom Virgo <tomcvirgo@gmail.com>
2021-07-05 10:03:46 +0100

    add GET route /journals

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const cors = require('cors');
-const { saveData, getData } = require('./utils')
+const { saveData, getData } = require('./utils');
+const Journal = require('./models');
 const app = express();
 
 //MIDDLEWARE
 app.use(express.json());
 app.use(cors());
 
-//ROUTES
+//GENERAL ROUTES
 app.get('/', (req,res) => {
     res.json('Server is up and running - woop!!');
 });
+
+//JOURNAL ROUTES
+
+//Use this route to get all of the journal entries that are saved in the journals.json file.
+app.get('/journals', (req,res) => {
+    const journals = getData();
+    res.json(journals);
+})
 
 module.exports = { app };
 
