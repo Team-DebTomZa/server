67efef4742edfbeaff680c73158e997b66bd0e26
Tom Virgo <tomcvirgo@gmail.com>
2021-07-06 14:01:07 +0100

    add gifUrl to model

--- models.js
+++ models.js
@@ -1,9 +1,11 @@
 class Journal{
-    constructor(id, title, content){
+    constructor(id, title, content, gifUrl){
         let date = new Date();
+        
         this.id = id;
         this.title = title;
         this.content = content;
+        this.gifUrl = gifUrl;
         this.emojis = [0, 0, 0]; //array with each element representing number of that emoji reactions the journal has
         this.comments = [];
         this.date = date.toLocaleDateString();
