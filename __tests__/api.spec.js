d1615a17b1fa72f38c0ccfb8c4321dd3bbb34b3e
Tom Virgo <tomcvirgo@gmail.com>
2021-07-12 17:47:48 +0100

    add tests

--- __tests__/api.spec.js
+++ __tests__/api.spec.js
@@ -3,26 +3,65 @@ const { app }= require('../app');
 
 describe('API server', () => {
     let api;
+
     let testEntry = {
         title: 'blog1',
         content: 'i was going for a long long walk...'
     };
+
+    let testNoTitle = {
+        content: 'how I broke into coding...'
+    };
+
+    let updateData = {
+        "emojis": [1,2,3],
+        "newComment": "Great Post!"
+    }
+
     let port = 5000;
 
     beforeAll(()=>{
         api = app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
     })
 
+    test('Get request to root url responds with status 200', (done) => {
+        request(api).get('/').expect(200,done);
+    })
+
     test('Get items responds with status 200', (done)=>{
         request(api).get('/journals').expect(200,done);
     })
 
-    test('Post a new entry responds with status 201', (done) => {
+    test('Posting a new entry responds with status 201', (done) => {
         request(api)
         .post('/journals')
         .send(testEntry)
         .set('Accept', /application\/json/)
         .expect(201,done)
+    })
+
+    test('Posting a new entry with no title responds with status 401', (done) => {
+        request(api)
+        .post('/journals')
+        .send(testNoTitle)
+        .set('Accept', /application\/json/)
+        .expect(401,done)
+    })
+
+    test('Updating an entry responds with status 200', (done) => {
+        request(api)
+        .patch('/journals/1')
+        .send(updateData)
+        .set('Accept', /application\/json/)
+        .expect(200,done)
+    })
+
+    test('Trying to update an entry with an invalid journal id responds with status 409', (done) => {
+        request(api)
+        .patch('/journals/100000')
+        .send(updateData)
+        .set('Accept', /application\/json/)
+        .expect(409,done)
     })
 
     test('responds to deleteall with status 204', async () => {
